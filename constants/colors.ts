e6e32e271c48231993411258d8320c6f35120092
Plínio Mayer <pctmayer@gmail.com>
2024-12-28 09:04:41 -0300

    fix: icone

--- constants/colors.ts
+++ constants/colors.ts
@@ -3,7 +3,7 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColor = "#24d978";
+const tintColor = "#24D978";
 
 export const Colors = {
   text: tintColor,
